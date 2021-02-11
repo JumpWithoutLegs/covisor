6e2c640b0359f5828e8325fdfa6fad98d6980784
Povlik Gidravlik <sorokinpublic@gmail.com>
2021-02-11 05:51:10 +0300

    added auth fragment, some images, dependencies and other standard boilerplate code

--- app/src/main/java/com/pavelgostudy/startandroid/covisor/MainActivity.java
+++ app/src/main/java/com/pavelgostudy/startandroid/covisor/MainActivity.java
@@ -1,14 +1,18 @@
 package com.pavelgostudy.startandroid.covisor;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
 public class MainActivity extends AppCompatActivity {
+    NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
     }
 }
